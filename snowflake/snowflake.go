b7df5b9d9cfada482f37d8355981e1e59588548f
don.wang <wanguandong@126.com>
2023-07-26 17:17:49 +0800

    perf: snowflake

--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -1,70 +1,32 @@
 package snowflake
 
 import (
-	"errors"
-	"sync"
+	"log"
 	"time"
+
+	"github.com/bwmarrin/snowflake"
 )
 
-const (
-	workerBits  uint8 = 10
-	numberBits  uint8 = 12
-	workerMax   int64 = -1 ^ (-1 << workerBits)
-	numberMax   int64 = -1 ^ (-1 << numberBits)
-	timeShift   uint8 = workerBits + numberBits
-	workerShift uint8 = numberBits
-	startTime   int64 = 1525705533000 // 如果在程序跑了一段时间修改了epoch这个值 可能会导致生成相同的ID
-)
-
-// Snowflake struct
-type Snowflake struct {
-	mu        sync.Mutex
-	timestamp int64
-	workerID  int64
-	number    int64
-}
+var _snowflake *snowflake.Node = nil
 
 // New 创建实例
-func New(workerID int64) (*Snowflake, error) {
-	if workerID < 0 || workerID > workerMax {
-		return nil, errors.New("worker id excess of quantity")
+func New(startTime string, machineID int64) {
+	var st time.Time
+	// 格式化 1月2号下午3时4分5秒  2006年
+	st, err := time.Parse("2006-01-02", startTime)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	// 生成一个新节点
-	return &Snowflake{
-		timestamp: 0,
-		workerID:  workerID,
-		number:    0,
-	}, nil
-}
-
-// NextId 下一个ID
-func (w *Snowflake) NextId() uint {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	now := time.Now().UnixNano() / 1e6
-	if w.timestamp == now {
-		w.number++
-		if w.number > numberMax {
-			for now <= w.timestamp {
-				now = time.Now().UnixNano() / 1e6
-			}
-		}
-	} else {
-		w.number = 0
-		w.timestamp = now
+	snowflake.Epoch = st.UnixNano() / 1e6
+	if _snowflake, err = snowflake.NewNode(machineID); err != nil {
+		log.Fatalln(err)
 	}
-	Id := uint((now-startTime)<<timeShift | (w.workerID << workerShift) | (w.number))
-	return Id
 }
-
-var (
-	_snowflake *Snowflake = nil
-)
 
 func init() {
-	_snowflake, _ = New(0x01)
+	New("2021-12-03", 0x01)
 }
 
-func PrimaryKey() uint {
-	return _snowflake.NextId()
+func NextID() uint {
+	return uint(_snowflake.Generate().Int64())
 }
