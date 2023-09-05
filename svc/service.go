82fd1bd59e9f4cfffe0e0ed5d6dc25a22cecb288
don.wang <wanguandong@126.com>
2023-09-05 14:18:14 +0800

    feat: coredump

--- svc/service.go
+++ svc/service.go
@@ -3,6 +3,10 @@ package svc
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+	"unsafe"
 
 	"github.com/kardianos/service"
 )
@@ -22,7 +26,22 @@ func (p *Program) Stop(s service.Service) error {
 	return p.Shutdown()
 }
 
+var tracefile = filepath.Dir(os.Args[0]) + "/" + time.Now().Format("20060102150405") + ".dump"
+
 func Run(p *Program) {
+	defer func() {
+		logFile, err := os.OpenFile(tracefile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+		if err == nil {
+			log.SetOutput(logFile) // 将文件设置为log输出的文件
+			log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+		}
+		if err := recover(); err != nil {
+			const size = 64 << 10
+			buf := make([]byte, size)
+			buf = buf[:runtime.Stack(buf, false)]
+			log.Fatalf("runtime error: %v\ntraceback:\n%v\n", err, *(*string)(unsafe.Pointer(&buf)))
+		}
+	}()
 	app := p.AppName()
 	s, err := service.New(p, &service.Config{
 		Name:        app,
@@ -45,5 +64,4 @@ func Run(p *Program) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
