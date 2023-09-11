2c022ea4a90588673a13ad2309b96f84f21b449f
don.wang <wanguandong@126.com>
2023-09-11 09:52:38 +0800

    perf: 返回分区时间以便过滤不合法时间数据

--- orm/partition.go
+++ orm/partition.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	timeFormat = "2006-01-02 15:04:05"
-	partFormat = "20060102 150405"
+	timeFormat = "2006-01-02"
+	partFormat = "20060102"
 )
 
 type schemaPartition struct {
@@ -23,12 +23,14 @@ type schemaPartition struct {
 type partition struct {
 	// 表名
 	Table string
+	// 保留分区数
+	Reserved int
 }
 
 func (o *partition) schemaName(t time.Time) (string, string) {
-	name := "p" + t.Format(partFormat)[:8]
-	where := t.AddDate(0, 0, 1).Format(timeFormat)[:10]
-	return name, where
+	name := "p" + t.Format(partFormat)
+	lessDay := t.AddDate(0, 0, 1).Format(timeFormat)
+	return name, lessDay
 }
 
 const queryPart = `SELECT PARTITION_NAME, PARTITION_DESCRIPTION, PARTITION_EXPRESSION FROM information_schema.PARTITIONS WHERE table_name = '%s' ORDER BY PARTITION_NAME DESC;`
@@ -36,39 +38,36 @@ const initPart = `ALTER TABLE %s PARTITION BY RANGE (TO_DAYS(%s))(PARTITION %s V
 const dropPart = `ALTER TABLE %s DROP PARTITION %s;`
 const createPart = `ALTER TABLE %s ADD PARTITION(PARTITION %s VALUES LESS THAN (TO_DAYS('%s')));`
 
-func (o *partition) queryAll(data interface{}) error {
-	return _db.Raw(fmt.Sprintf(queryPart, o.Table)).Scan(data).Error
+func (p *partition) queryAll(data interface{}) error {
+	return _db.Raw(fmt.Sprintf(queryPart, p.Table)).Scan(data).Error
 }
 
-func (o *partition) AlterRange(rDays, interval int, field string) {
-	t := time.Now()
-	pname, lessDay := o.schemaName(t)
-	var data []schemaPartition
-	o.queryAll(&data)
+// 返回当前最新分区日期，时间过滤
+func (p *partition) AlterRange(rDays, interval int, field string) string {
+	data, t0 := []schemaPartition{}, time.Now()
+	p.queryAll(&data)
 	if data[0].Name == "" {
-		_db.Exec(fmt.Sprintf(initPart, o.Table, field, pname, lessDay))
-		data[0].Name = pname
-		return
+		name, lessDay := p.schemaName(t0)
+		_db.Exec(fmt.Sprintf(initPart, p.Table, field, name, lessDay)) // 初始化分区
+		data[0].Name = name
 	}
 	num := len(data)
 	if num*interval > rDays {
-		_db.Exec(fmt.Sprintf(dropPart, o.Table, data[num-1].Name)) // 删除过时分区
+		_db.Exec(fmt.Sprintf(dropPart, p.Table, data[num-1].Name)) // 删除过时分区
 	}
-	if data[0].Name > pname {
-		return
+	t1, _ := time.Parse(partFormat, data[0].Name[1:])
+	if t0.AddDate(0, 0, interval*p.Reserved).Compare(t1.AddDate(0, 0, 1)) <= 0 {
+		return t1.Format(timeFormat) // 返回最新分区日期
 	}
-	// 中途启动创建当前分区
-	if data[0].Name < pname {
-		_db.Exec(fmt.Sprintf(createPart, o.Table, pname, lessDay))
-	}
-	nextp, nextLessDay := o.schemaName(t.AddDate(0, 0, interval))
-	_db.Exec(fmt.Sprintf(createPart, o.Table, nextp, nextLessDay))
+	// note, 这里再最新分区基础上interval添加新分区
+	nextp, nextLessDay := p.schemaName(t1.AddDate(0, 0, interval))
+	_db.Exec(fmt.Sprintf(createPart, p.Table, nextp, nextLessDay))
+	return nextLessDay
 }
 
 func Partition(m interface{}) *partition {
-	v, ok := m.(schema.Tabler)
-	if ok {
-		return &partition{Table: v.TableName()}
+	if v, ok := m.(schema.Tabler); ok {
+		return &partition{Table: v.TableName(), Reserved: 2}
 	}
-	panic(fmt.Errorf("%v typeOf not gorm schema.Tabler", reflect.TypeOf(v)))
+	panic(fmt.Errorf("%v typeOf not gorm schema.Tabler", reflect.TypeOf(m)))
 }
