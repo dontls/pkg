0d8255ca2b09e2ca56d96db95f67a75affddbf75
don.wang <wanguandong@126.com>
2024-10-22 16:43:44 +0800

    perf: 字符串类型in

--- sqlr/sqlc.go
+++ sqlr/sqlc.go
@@ -15,76 +15,97 @@ func NewSQL(s string) *sqlc {
 	return &sqlc{sqls: s}
 }
 
-func (o *sqlc) Where(query string) *sqlc {
+func (s *sqlc) Where(query string) *sqlc {
 	if query != "" {
-		o.wheres = append(o.wheres, query)
-	}
-	return o
-}
-
-func (o *sqlc) Equal(query string, v interface{}) *sqlc {
-	switch v := v.(type) {
-	case string:
-		if v != "" {
-			o.wheres = append(o.wheres, fmt.Sprintf("%s = '%s'", query, v))
-		}
-	case int:
-		o.wheres = append(o.wheres, fmt.Sprintf("%s = %d", query, v))
-	}
-	return o
-}
-
-func (o *sqlc) In(query, s string) *sqlc {
-	if s != "" {
-		s := (query + " IN(" + s + ")")
-		o.wheres = append(o.wheres, s)
-	}
-	return o
-}
-
-func (o *sqlc) InStrings(query, s string) *sqlc {
-	if s != "" {
-		s := (query + " IN('" + strings.ReplaceAll(s, ",", "','") + "')")
-		o.wheres = append(o.wheres, s)
-	}
-	return o
-}
-
-func (o *sqlc) Between(query string, ss ...string) *sqlc {
-	if len(ss) == 2 {
-		s := (query + " BETWEEN '" + ss[0] + "' AND '" + ss[1] + "'")
-		o.wheres = append(o.wheres, s)
-	}
-	return o
-}
-
-func (o *sqlc) Limit(num, size int) *sqlc {
-	if size > 0 {
-		o.others = append(o.others, fmt.Sprintf("LIMIT %d,%d", (num-1)*size, size))
-	}
-	return o
-}
-
-func (o *sqlc) OrderBy(query string, s string) *sqlc {
-	o.others = append(o.others, "ORDER BY "+query+" "+s)
-	return o
-}
-
-func (o *sqlc) GroupBy(query string) *sqlc {
-	o.others = append(o.others, "GROUP BY "+query)
-	return o
-}
-
-func (o *sqlc) Select(s string) string {
-	if len(o.wheres) > 0 {
-		s += (" WHERE " + strings.Join(o.wheres, " AND "))
-	}
-	if len(o.others) > 0 {
-		s += (" " + strings.Join(o.others, " "))
+		s.wheres = append(s.wheres, query)
 	}
 	return s
 }
 
-func (o *sqlc) String() string {
-	return o.Select(o.sqls)
+type String string
+
+func (s *sqlc) Equal(query string, v any) *sqlc {
+	s1 := ""
+	switch v := v.(type) {
+	case string:
+		if v != "" {
+			s1 = fmt.Sprintf("%s='%s'", query, v)
+		}
+	case String:
+		v1 := string(v)
+		if v1 != "" {
+			s1 = fmt.Sprintf("%s='%s'", query, v1)
+		}
+	case int:
+		s1 = fmt.Sprintf("%s=%d", query, v)
+	}
+	if s1 != "" {
+		s.wheres = append(s.wheres, s1)
+	}
+	return s
+}
+
+// 1,2,3
+// ab,cd,ef，类型使用sqlr.String
+// ["ab", "cd"]
+func (s *sqlc) In(query string, v any) *sqlc {
+	s1 := ""
+	switch v := v.(type) {
+	case string:
+		if v != "" {
+			s1 = (query + " IN(" + v + ")")
+		}
+	case String:
+		v1 := string(v)
+		if v1 != "" {
+			s1 = (query + " IN('" + strings.ReplaceAll(v1, ",", "','") + "')")
+		}
+	case []string:
+		if len(v) > 0 {
+			s1 = (query + " IN('" + strings.Join(v, "','") + "')")
+		}
+	}
+	if s1 != "" {
+		s.wheres = append(s.wheres, s1)
+	}
+	return s
+}
+
+func (s *sqlc) Between(query string, ss ...string) *sqlc {
+	if len(ss) == 2 {
+		s1 := (query + " BETWEEN '" + ss[0] + "' AND '" + ss[1] + "'")
+		s.wheres = append(s.wheres, s1)
+	}
+	return s
+}
+
+func (s *sqlc) Limit(num, size int) *sqlc {
+	if size > 0 {
+		s.others = append(s.others, fmt.Sprintf("LIMIT %d,%d", (num-1)*size, size))
+	}
+	return s
+}
+
+func (s *sqlc) OrderBy(query string, v string) *sqlc {
+	s.others = append(s.others, "ORDER BY "+query+" "+v)
+	return s
+}
+
+func (s *sqlc) GroupBy(query string) *sqlc {
+	s.others = append(s.others, "GROUP BY "+query)
+	return s
+}
+
+func (s *sqlc) Select(v string) string {
+	if len(s.wheres) > 0 {
+		v += (" WHERE " + strings.Join(s.wheres, " AND "))
+	}
+	if len(s.others) > 0 {
+		v += (" " + strings.Join(s.others, " "))
+	}
+	return v
+}
+
+func (s *sqlc) String() string {
+	return s.Select(s.sqls)
 }
